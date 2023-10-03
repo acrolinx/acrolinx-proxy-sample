a88591b04cfd94ac4fb6ba4be6f756a491c06973
Thilo Mühlberg <thilo.muehlberg@acrolinx.com>
2023-10-03 09:23:35 +0200

    Configure spotless to use googleJavaFormat(), reformat all Java code

--- java/proxy-sample/src/test/java/com/acrolinx/proxy/DotenvUtil.java
+++ java/proxy-sample/src/test/java/com/acrolinx/proxy/DotenvUtil.java
@@ -1,25 +1,21 @@
-
 package com.acrolinx.proxy;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-final class DotenvUtil
-{
-    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+final class DotenvUtil {
+  private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-    static String getEnvironmentVariable(String variableKey)
-    {
-        String variableValue = dotenv.get(variableKey);
+  static String getEnvironmentVariable(String variableKey) {
+    String variableValue = dotenv.get(variableKey);
 
-        if (variableValue == null) {
-            throw new IllegalArgumentException("missing environment variable: " + variableKey);
-        }
-
-        return variableValue;
+    if (variableValue == null) {
+      throw new IllegalArgumentException("missing environment variable: " + variableKey);
     }
 
-    private DotenvUtil()
-    {
-        throw new IllegalStateException();
-    }
+    return variableValue;
+  }
+
+  private DotenvUtil() {
+    throw new IllegalStateException();
+  }
 }
