483b3b739970b40004052823c5157944ef092685
Danjela Lura <danjela.lura@acrolinx.com>
2024-01-11 15:10:55 +0100

    DEV-35542 chore: Remove unused dependency

--- src/test/java/com/acrolinx/proxy/util/HttpMethod.java
+++ src/test/java/com/acrolinx/proxy/util/HttpMethod.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.NotImplementedException;
 
 public enum HttpMethod {
   DELETE,
