2419ca3b1bec1057dfe767a7a24897dbd30fb21f
Danjela Lura <danjela.lura@acrolinx.com>
2024-01-17 12:21:32 +0100

    Add private helper method in ServletConfigUtil

--- src/test/java/com/acrolinx/proxy/util/ServletConfigUtil.java
+++ src/test/java/com/acrolinx/proxy/util/ServletConfigUtil.java
@@ -6,20 +6,23 @@ import org.mockito.Mockito;
 
 class ServletConfigUtil {
   static void stubServletConfigBase(ServletConfig servletConfig, String acrolinxUrl) {
-    Mockito.when(servletConfig.getInitParameter("acrolinxURL")).thenReturn(acrolinxUrl);
-    Mockito.when(servletConfig.getInitParameter("genericToken")).thenReturn("token");
-    Mockito.when(servletConfig.getInitParameter("username")).thenReturn("username");
+    stubInitParameter(servletConfig, "acrolinxURL", acrolinxUrl);
+    stubInitParameter(servletConfig, "genericToken", "token");
+    stubInitParameter(servletConfig, "username", "username");
   }
 
   static void stubServletConfigConnectTimeout(
       ServletConfig servletConfig, String connectTimeoutInMillis) {
-    Mockito.when(servletConfig.getInitParameter("connectTimeoutInMillis"))
-        .thenReturn(connectTimeoutInMillis);
+    stubInitParameter(servletConfig, "connectTimeoutInMillis", connectTimeoutInMillis);
   }
 
   static void stubServletConfigSocketTimeout(
       ServletConfig servletConfig, String socketTimeoutInMillis) {
-    Mockito.when(servletConfig.getInitParameter("socketTimeoutInMillis"))
-        .thenReturn(socketTimeoutInMillis);
+    stubInitParameter(servletConfig, "socketTimeoutInMillis", socketTimeoutInMillis);
+  }
+
+  private static void stubInitParameter(
+      ServletConfig servletConfig, String parameterName, String parameterValue) {
+    Mockito.when(servletConfig.getInitParameter(parameterName)).thenReturn(parameterValue);
   }
 }
