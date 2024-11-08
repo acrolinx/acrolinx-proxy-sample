15da0af3ec03a0d4975615697c40a84829fc8bb5
Thilo Mühlberg <thilo.muehlberg@acrolinx.com>
2024-11-08 12:41:03 +0100

    Execute maven-failsafe-plugin:verify goal too, add unit test timeout

--- src/test/java/com/acrolinx/proxy/TomcatIT.java
+++ src/test/java/com/acrolinx/proxy/TomcatIT.java
@@ -103,7 +103,7 @@ class TomcatIT {
   }
 
   @Test
-  void embeddedTomcatTest(@TempDir Path tempDirectory) throws Exception {
+  void integrationTest(@TempDir Path tempDirectory) throws Exception {
     try (WireMockServerWrapper wireMockServerWrapper = WireMockServerWrapper.startOnHttpPort(8031);
         TomcatWrapper tomcatWrapper = TomcatWrapper.startOnRandomHttpPort(tempDirectory)) {
       final WireMockServer wireMockServer = wireMockServerWrapper.getWireMockServer();
